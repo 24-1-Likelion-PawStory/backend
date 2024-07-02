2e0518c0ce30a4ec3b61fd6034ec84bd7a3d74af
kaswhy <sxshx818@naver.com>
2024-07-03 02:15:08 +0900

    fix: 일기 관련 내용 수정

--- pawStory/diaries/serializers.py
+++ pawStory/diaries/serializers.py
@@ -1,18 +1,18 @@
 from rest_framework import serializers
-from .models import *
+from .models import Diary, DiaryLike, DiaryComment, Follow
 from users.models import Member
 
 class MemberDiarySerializer(serializers.ModelSerializer):
     class Meta:
         model = Member
-        fields = ['id', 'user_id', 'profile_image']  # 필요 시 추가 필드
+        fields = ['id', 'user_id', 'pet_photo']
 
 class DiaryCommentSerializer(serializers.ModelSerializer):
     member = MemberDiarySerializer(read_only=True)
     
     class Meta:
         model = DiaryComment
-        fields = ['id', 'content', 'created_at', 'member']
+        fields = ['id', 'content', 'member']
 
 class DiaryLikeSerializer(serializers.ModelSerializer):
     member = MemberDiarySerializer(read_only=True)
@@ -21,16 +21,24 @@ class DiaryLikeSerializer(serializers.ModelSerializer):
         model = DiaryLike
         fields = ['id', 'member']
 
+class DiaryCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Diary
+        fields = ['photo', 'content', 'is_public']  # member 필드 제외
+
+    def create(self, validated_data):
+        member = self.context['request'].user
+        return Diary.objects.create(member=member, **validated_data)
+
 class DiarySerializer(serializers.ModelSerializer):
     member = MemberDiarySerializer(read_only=True)
-    likes = DiaryLikeSerializer(many=True, read_only=True, source='diary_likes')
     comments = DiaryCommentSerializer(many=True, read_only=True, source='diary_comments')
     like_count = serializers.IntegerField(source='diary_likes.count', read_only=True)
     comment_count = serializers.IntegerField(source='diary_comments.count', read_only=True)
 
     class Meta:
         model = Diary
-        fields = ['id', 'photo', 'content', 'created_at', 'is_public', 'member', 'likes', 'comments', 'like_count', 'comment_count']
+        fields = ['id', 'photo', 'content', 'created_at', 'is_public', 'member', 'comments', 'like_count', 'comment_count']
 
 class DiaryListSerializer(serializers.ModelSerializer):
     class Meta:
--- pawStory/diaries/views.py
+++ pawStory/diaries/views.py
@@ -1,74 +1,42 @@
 from rest_framework import generics, status
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
-from .models import *
-from .serializers import *
-from django.shortcuts import get_object_or_404
+from .models import Diary, DiaryLike, DiaryComment, Follow
+from .serializers import DiaryCreateSerializer, DiarySerializer, DiaryListSerializer, DiaryLikeSerializer, DiaryCommentSerializer, FollowSerializer
 
-def api_response(data, message, status_code):
-    response = {
-        "message": message,
-        "data": data
-    }
-    return Response(response, status=status_code)
+class DiaryCreateView(generics.CreateAPIView):
+    queryset = Diary.objects.all()
+    serializer_class = DiaryCreateSerializer
+    permission_classes = [IsAuthenticated]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def perform_create(self, serializer):
+        serializer.save()
 
 class DiaryListView(generics.ListAPIView):
-    queryset = Diary.objects.all()
+    queryset = Diary.objects.all().order_by('-created_at')
     serializer_class = DiaryListSerializer
-
-    def list(self, request, *args, **kwargs):
-        response = super().list(request, *args, **kwargs)
-        return api_response(serializer.data, "일기 목록 조회 성공", status.HTTP_200_OK)
+    permission_classes = [IsAuthenticated]
 
 class DiaryDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Diary.objects.all()
     serializer_class = DiarySerializer
     permission_classes = [IsAuthenticated]
 
-    def get(self, request, *args, **kwargs):
-        response = super().retrieve(request, *args, **kwargs)
-        return api_response(data=response.data, message="일기 조회 성공", status_code=status.HTTP_200_OK)
-
-    def put(self, request, *args, **kwargs):
-        response = super().update(request, partial=partial, *args, **kwargs)
-        return api_response(data=response.data, message="일기 수정 성공", status_code=status.HTTP_200_OK)
-
-    def patch(self, request, *args, **kwargs):
-        response = super().partial_update(request, partial=partial, *args, **kwargs)
-        return api_response(data=response.data, message="일기 부분 수정 성공", status_code=status.HTTP_200_OK)
-
-    def delete(self, request, *args, **kwargs):
-        instance = self.get_object()
-        self.perform_destroy(instance)
-        return api_response(None, "일기 삭제 성공", status_code=status.HTTP_204_NO_CONTENT)
-
-    def perform_destroy(self, instance):
-        # 일기와 관련된 좋아요와 댓글 모두 삭제
-        DiaryLike.objects.filter(diary=instance).delete()
-        DiaryComment.objects.filter(diary=instance).delete()
-        instance.delete()
-
-class DiaryCreateView(generics.CreateAPIView):
-    queryset = Diary.objects.all()
-    serializer_class = DiarySerializer
-    permission_classes = [IsAuthenticated]
-
-    def post(self, request, *args, **kwargs):
-        response = super().create(request, *args, **kwargs)
-        return api_response(data=response.data, message="일기 작성 성공", status_code=status.HTTP_201_CREATED)
-
 class DiaryLikeCreateView(generics.CreateAPIView):
     queryset = DiaryLike.objects.all()
     serializer_class = DiaryLikeSerializer
     permission_classes = [IsAuthenticated]
 
-    def post(self, request, *args, **kwargs):
-        diary = get_object_or_404(Diary, id=self.kwargs['id'])
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
+    def perform_create(self, serializer):
+        diary = Diary.objects.get(pk=self.kwargs['id'])
         serializer.save(member=self.request.user, diary=diary)
-        response = super().create(request, *args, **kwargs)
-        return api_response(data=response.data, message="좋아요 성공", status_code=status.HTTP_201_CREATED)
 
 class DiaryLikeDeleteView(generics.DestroyAPIView):
     queryset = DiaryLike.objects.all()
@@ -76,11 +44,7 @@ class DiaryLikeDeleteView(generics.DestroyAPIView):
     permission_classes = [IsAuthenticated]
 
     def get_object(self):
-        return get_object_or_404(DiaryLike, member=self.request.user, diary__id=self.kwargs['id'])
-
-    def delete(self, request, *args, **kwargs):
-        response = super().delete(request, *args, **kwargs)
-        return api_response(None, "좋아요 취소 성공", status_code=status.HTTP_204_NO_CONTENT)
+        return DiaryLike.objects.get(diary_id=self.kwargs['id'], member=self.request.user)
 
 class DiaryCommentCreateView(generics.CreateAPIView):
     queryset = DiaryComment.objects.all()
@@ -88,25 +52,16 @@ class DiaryCommentCreateView(generics.CreateAPIView):
     permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
-        diary = get_object_or_404(Diary, id=self.kwargs['id'])
+        diary = Diary.objects.get(pk=self.kwargs['id'])
         serializer.save(member=self.request.user, diary=diary)
-
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        return api_response(serializer.data, "댓글 작성 성공", status.HTTP_201_CREATED)
 
 class DiaryCommentListView(generics.ListAPIView):
     serializer_class = DiaryCommentSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        return DiaryComment.objects.filter(diary__id=self.kwargs['id'])
-
-    def list(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-        serializer = self.get_serializer(queryset, many=True)
-        return api_response(serializer.data, "댓글 목록 조회 성공", status.HTTP_200_OK)
+        diary_id = self.kwargs['id']
+        return DiaryComment.objects.filter(diary_id=diary_id)
 
 class DiaryCommentDeleteView(generics.DestroyAPIView):
     queryset = DiaryComment.objects.all()
@@ -114,11 +69,7 @@ class DiaryCommentDeleteView(generics.DestroyAPIView):
     permission_classes = [IsAuthenticated]
 
     def get_object(self):
-        return get_object_or_404(DiaryComment, id=self.kwargs['comment_id'], member=self.request.user)
-
-    def delete(self, request, *args, **kwargs):
-        response = super().delete(request, *args, **kwargs)
-        return api_response(None, "댓글 삭제 성공", status_code=status.HTTP_204_NO_CONTENT)
+        return DiaryComment.objects.get(pk=self.kwargs['comment_id'], diary_id=self.kwargs['id'])
 
 class FollowCreateView(generics.CreateAPIView):
     queryset = Follow.objects.all()
@@ -126,15 +77,9 @@ class FollowCreateView(generics.CreateAPIView):
     permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
-        following_id = self.request.data.get('following')
-        following = get_object_or_404(Member, id=following_id)
-        serializer.save(follower=self.request.user, following=following)
-
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        return api_response(serializer.data, "팔로우 성공", status.HTTP_201_CREATED)
+        follower = self.request.user
+        following = Member.objects.get(pk=self.request.data['following'])
+        serializer.save(follower=follower, following=following)
 
 class FollowDeleteView(generics.DestroyAPIView):
     queryset = Follow.objects.all()
@@ -142,9 +87,6 @@ class FollowDeleteView(generics.DestroyAPIView):
     permission_classes = [IsAuthenticated]
 
     def get_object(self):
-        following_id = self.request.data.get('following')
-        return get_object_or_404(Follow, follower=self.request.user, following__id=following_id)
-
-    def delete(self, request, *args, **kwargs):
-        response = super().delete(request, *args, **kwargs)
-        return api_response(None, "언팔로우 성공", status_code=status.HTTP_204_NO_CONTENT)
+        follower = self.request.user
+        following = Member.objects.get(pk=self.request.data['following'])
+        return Follow.objects.get(follower=follower, following=following)
