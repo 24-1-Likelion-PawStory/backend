0a9b8837beb79039a25129f7524329cdf7c218c7
kaswhy <sxshx818@naver.com>
2024-07-05 02:45:25 +0900

    feat: static 관련 url 설정 추가

--- pawStory/pawStory/urls.py
+++ pawStory/pawStory/urls.py
@@ -1,12 +1,12 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-# 스웨거 설정하느라 추가
 from django.urls import path, include, re_path
 from rest_framework.permissions import AllowAny
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+# Swagger 설정
 schema_view = get_schema_view(
     openapi.Info(
         title="프로젝트 이름(예: likelion-project)",
@@ -20,15 +20,21 @@ schema_view = get_schema_view(
     permission_classes=(AllowAny,),
 )
 
+# URL 패턴
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('community/', include('community.urls')),
     path('diaries/', include('diaries.urls')),
     path('users/', include('users.urls')),
     path('accounts/', include('accounts.urls')),
-    # 스웨거
+    # Swagger
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # 미디어 파일을 제공할 수 있도록 URL 패턴 추가
+]
 
+# 미디어 파일 제공 설정
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# 정적 파일 제공 설정
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
