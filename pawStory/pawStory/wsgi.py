c71d345e6316a5a70d6417b2bedbdf0105b2f3d3
kihyun <mable0927@gmail.com>
2024-07-05 21:17:52 +0900

    prodㅈ수ㅗ추가

--- pawStory/pawStory/wsgi.py
+++ pawStory/pawStory/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pawStory.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pawStory.settings.prod')
 
 application = get_wsgi_application()
